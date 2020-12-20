79985f78da04a83de01aa49b969d9546bea405c7
Gustavo Saboya <saboya.gustavo@gmail.com>
2020-12-20 17:13:14 -0300

    Implemented a function that writes a json for each state.

--- module02/practical-work2/index.js
+++ module02/practical-work2/index.js
@@ -1,11 +1,25 @@
 const fs = require('fs').promises;
 
-async function start() {
+function start() {
+    console.log('\n Program started.');
+    createFiles();
+
+    console.log('\n End of the process.');
+}
+
+async function createFiles() {
+    console.log('\nReading states file');
     let data = await fs.readFile('./files/Estados.json');
     const states = JSON.parse(data);
+
+    console.log('\nReading cities file');
     data = await fs.readFile('./files/Cidades.json');
     const cities = JSON.parse(data);
-    console.log(states);
-    console.log(cities);
+
+    console.log('\nCreating state files');
+    for (let state of states) {
+        const stateCities = cities.filter((city) => city.Estado === state.ID);
+        await fs.writeFile(`./files/states/${state.Sigla}.json`, JSON.stringify(stateCities));
+    }
 }
 start();
