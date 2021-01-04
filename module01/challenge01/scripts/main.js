e6201db403fba5724d8bcbc293b0f09fc67b5f64
Gustavo Saboya <saboya.gustavo@gmail.com>
2021-01-04 11:53:09 -0300

    Handling typing on input and setting the search function.

--- module01/challenge01/scripts/main.js
+++ module01/challenge01/scripts/main.js
@@ -1,4 +1,7 @@
 const path = 'https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo';
+const displayedUsers = [];
+let input = null;
+let inputName = null;
 
 async function fetchData(path) {
     const data = await fetch(path);
@@ -6,9 +9,42 @@ async function fetchData(path) {
     return json;
 }
 
-async function init() {
-    const data = (await fetchData(path)).results;
-    console.log(data);
+function init() {
+    console.log('Page loaded successfully.');
+    input = document.querySelector('#searchBar');
+    preventFormSubmit();
+    activateInput();
+}
+
+function preventFormSubmit() {
+    function handleFormSubmit(event) {
+        event.preventDefault();
+    }
+    let form = document.querySelector('form');
+    form.addEventListener('submit', handleFormSubmit);
+}
+
+function activateInput() {
+    async function searchName(name) {
+        inputName = name;
+        const data = (await fetchData(path)).results;
+        let result = data.filter((user) => {
+            return user.name.first === inputName;
+        });
+
+        console.log('Here is your result: ', result);
+        //console.log(data);
+    }
+
+    function handleTyping(event) {
+        if (event.key === 'Enter') {
+            console.log('searching...');
+            searchName(event.target.value);
+        }
+    }
+
+    input.focus();
+    input.addEventListener('keyup', handleTyping);
 }
 
 init();
